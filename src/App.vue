511f3f03929dbab89b3d6b09f58409d8c40a8a8c
Barbouy <pa.holliger@gmail.com>
2022-01-17 02:31:08 +0100

    It works, really great.

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,13 @@ export default { name: "App" }
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Poppins:wght@300;400;500;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Aleo:wght@300;400;700&family=Poppins:wght@300;400;500;700&display=swap');
 
 html, body, * {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
-  font-family: "Poppins", sans-serif;
+  font-family: "Aleo", sans-serif;
   font-weight: 500;
 }
 
--- src/views/Twitch.vue
+++ src/views/Twitch.vue
@@ -15,17 +15,43 @@ export default {
   name: "Twitch",
   components: { MessageCard },
   data() {
-    return { clientId: "udfhbi5d9rhth40svup39y9okkli6t" }
+    return {
+      clientId: "udfhbi5d9rhth40svup39y9okkli6t",
+      clientSecret: "7m3xbl6vl68rga1bu9zmiy7ve9yn8z",
+      token: "7h3rlr7c6x848p2yhty55w9ogps4cg",
+      userId: "115308944"
+    }
   },
   created() {
     this.login()
   },
   methods: {
     // https://dev.twitch.tv/docs/api
+    // https://dev.twitch.tv/docs/api/reference#get-followed-streams
     login() {
-      axios.get("https://api.twitch.tv/kraken/users/44322889", { headers: { "Client-ID": this.clientId } })
-        .then(response => console.log(response))
+      axios.get("https://api.twitch.tv/helix/users", {
+        headers: {
+          "Client-ID": this.clientId,
+          "Authorization": `Bearer ${this.token}` 
+        },
+        params: { "login": "barbouyy" } 
+      })
+        .then(response => console.log(response.data.data[0]))
         .catch(error => console.error(error))
+        .finally(() => this.follows())
+    },
+    follows() {
+      axios.get("https://api.twitch.tv/helix/users/follows", {
+        headers: { 
+          "Client-ID": this.clientId,
+          "Authorization": `Bearer ${this.token}` 
+        },
+        params: {
+          "to_id": this.userId,
+          "first": 5 
+        } 
+      })
+        .then(response => console.log(response.data))
     }
   }
 }
